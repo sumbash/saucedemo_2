23ff618da9330813e37316edcf6ba862592394df
sumbash <sumairabashir14@gmail.com>
2026-05-06 11:10:13 +0400

    CI/CD created

---
+++ tests/checkout.spec.ts
@@ -0,0 +1,30 @@
+import {test, expect} from '@playwright/test';
+import { LoginPage } from '../Pages/loginpage';
+import { baseUrl, Username, Password } from '../utils/envConfig';
+import { ProductPage } from '../Pages/productpage';
+
+test.describe('product page tests', () => {
+
+    let productPage: ProductPage;
+    let loginPage: LoginPage;
+
+    test.beforeEach(async ({ page }) => {
+        loginPage = new LoginPage(page);
+        productPage = new ProductPage(page);
+
+        await page.goto(baseUrl);
+        await page.waitForLoadState('networkidle');
+        await loginPage.login(Username, Password);
+        await expect(page).toHaveURL(/.*inventory\.html/);  
+    });
+
+ test('should logout successfully 1', async ({ page }) => {
+
+        await productPage.logout();
+        await expect(page.locator('#login-button')).toBeVisible(); 
+    }
+
+ )
+
+
+});
